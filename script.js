82316a1b6bde3ebfc4819056a8284e70fa6b7990
Eddy <eddyjohn.terosier@hotmail.com>
2022-11-08 13:25:19 +0100

    SELECTION SHOW MODAL WITH QUERYSELECTORALL

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-'use strict';
+"use strict";
+
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const btn = document.querySelector(".close-modal");
+
+// QUERYSELECTORALL pour selectionner plusieurs éléments qui ont la meme classe
+const btnsOpenModal = document.querySelectorAll(".show-modal");
+
+for (let i = 0; i < btnsOpenModal.length; i++) {
+  console.log(btnsOpenModal[i].textContent);
+}
