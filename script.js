d3cb74843db0edcc465db4cdeb6500e3a55dea12
Eddy <eddyjohn.terosier@hotmail.com>
2022-11-08 16:47:43 +0100

    BOUTON ECHAP

--- script.js
+++ script.js
@@ -21,4 +21,11 @@ for (let i = 0; i < btnsOpenModal.length; i++) {
   btnsOpenModal[i].addEventListener("click", openModal);
   btnCloseModal.addEventListener("click", closeModal);
   overlay.addEventListener("click", closeModal);
+  document.addEventListener("keydown",function(e){
+    if (e.key === "Escape") {
+      if (!modal.classList.contains("hidden")) {
+        closeModal("Escape");
+      }
+    }
+  } )
 }
